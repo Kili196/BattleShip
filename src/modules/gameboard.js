e7ea2011dfb970793139344fd9ee5f8886f45e14
kili196 <kilian.baier16@gmail.com>
2024-12-16 17:05:11 +0100

    added gameboard and ships

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,5 +1,14 @@
+const Ship = require("./ship");
+
 module.exports = class Gameboard {
     constructor() {
         this.gameboard = Array.from({ length: 100 }, () => []);
+
+        this.ship = {
+            "Carrier": new Ship("Carrier", 5),
+            "Battleship": new Ship("Carrier", 4),
+            "Carrier": new Ship("Carrier", 3),
+            "Submarine": new Ship("Carrier", 3),
+        }
     }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,12 +1,16 @@
 const gameBoardConstructor = require("../modules/gameboard")
 
-let gameBoard;
+let gameBoardObject;
 
 
 beforeEach(() => {
-    gameBoard = new gameBoardConstructor();
+    gameBoardObject = new gameBoardConstructor();
 })
 
 test("Test gameBoard construcotr", () => {
     expect(new gameBoardConstructor()).toBeDefined();
+})
+
+test("Test size of gameboard", () => {
+    expect(gameBoardObject.gameboard.length).toBe(100);
 })
