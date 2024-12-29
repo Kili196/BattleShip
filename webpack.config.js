6dd74156b33f89475929b58a7e30b16239809c2a
kili196 <kilian.baier16@gmail.com>
2024-12-29 14:02:39 +0100

    added template html and css loader to html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Custom template using Handlebars',
+            template: './src/index.html'
+        })
+    ]
 };
