a4004a37ba747712a7ba51cf1bc32f3b65a6abbd
kili196 <kilian.baier16@gmail.com>
2025-01-03 17:22:08 +0100

    added ship collection of buttons to gui

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import "./style.css"
 
 function getDomElements() {
     const gameBoards = document.getElementsByClassName("game-boards")[0];
-
-
     return ({ gameBoards })
 }
 
 
 function createGameBoardGui() {
-    const gameBoards = getDomElements().gameBoards;
 
+    const gameBoards = getDomElements().gameBoards;
     const gameBoard = document.createElement("div");
+    const gameBoardContainer = document.createElement("div");
     gameBoard.className = "gameBoard";
+    gameBoardContainer.classList = "gameBoardContainer";
+
+    const ships = ["Carrier", "Submarine", "Battleship"];
+
+
+
 
     for (let i = 1; i < 11; i++) {
         let gameBoardRow = document.createElement("div");
@@ -28,7 +33,23 @@ function createGameBoardGui() {
         gameBoard.appendChild(gameBoardRow);
     }
 
-    gameBoards.appendChild(gameBoard);
+    gameBoardContainer.appendChild(gameBoard);
+
+
+    const shipSelectionContainer = document.createElement("div");
+    shipSelectionContainer.className = "shipSelectionContainer";
+
+    ships.forEach((element) => {
+        const shipButton = document.createElement("button");
+        shipButton.textContent = element;
+        shipButton.className = "shipButton";
+
+        shipSelectionContainer.appendChild(shipButton);
+    })
+
+    gameBoardContainer.appendChild(shipSelectionContainer);
+
+    gameBoards.appendChild(gameBoardContainer);
 
 
 
